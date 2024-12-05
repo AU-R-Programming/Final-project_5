fead4ef765bc2fd531e6c9dc0866a2f1e038589c
Ifesinachi Aroh <arohifesinachi@gmail.com>
2024-12-04 23:53:55 -0600

    Updated the Description file and test.R file

--- test.R
+++ test.R
@@ -1,4 +1,50 @@
-# Testing the code
+# Testing the functions on a dataset provided on canvas
+# Load the dataset - We used the Bank Marketing data set provided
+data <- read.csv("~/Downloads/bank.csv", sep = ";")
+
+# Convert target variable to binary (0 = "no", 1 = "yes")
+data$y <- ifelse(data$y == "yes", 1, 0)
+
+# Select columns
+numerical_columns <- c("age", "balance", "duration", "previous")
+data_subset <- data[, c(numerical_columns, "y")]
+
+# Standardize numerical features (mean = 0, sd = 1)
+X_numeric <- scale(data_subset[, numerical_columns])
+
+# Add intercept column to the design matrix
+X <- as.matrix(cbind(1, X_numeric))
+
+# Ensure the response variable (y) is numeric
+y <- as.numeric(data_subset$y)
+
+# estimate coefficients
+beta <- estimate_beta(X, y)
+print("Estimated Coefficients:")
+print(beta)
+
+# calculating bootstrap confidence intervals
+bootstrap_results <- bootstrap_co_int(X, y)
+print("Bootstrap Confidence Intervals:")
+print(bootstrap_results)
+
+#Predicted Probablities
+pred_probs <- predicted_prob(beta, X)
+head(pred_probs)
+
+#predictions
+predictions <- predict(beta, X)
+head(predictions)
+
+
+# calculating confusion matrix and metrics
+metrics <- confusion_matrix_metrics(y, predictions)
+print("Confusion Matrix Metrics:")
+print(metrics)
+
+
+
+# Testing the function with simulated data
 set.seed(5) # Group name
 n <- 100
 X <- cbind(1, matrix(rnorm(n * 2), ncol = 2))
